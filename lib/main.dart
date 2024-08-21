2782ec5c10e4cdae2ed68be055bac9ba9f34b17d
Carlos Luan <carlos.luanrodrigues@outlook.com>
2024-08-21 20:40:45 -0300

    'site'

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 111, 23, 23)),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: const Landingpage(),
     );
